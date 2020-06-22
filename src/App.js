1ede06c88755735eabafb772d7bc20fdcdf07629
cesgarci1 <cesgarci1@publicisgroupe.net>
2020-06-21 22:31:32 -0500

    Chapter 12 before turning class components to functions

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import './App.css';
 
 class App extends Component {
   render() {
-    const name = "John Doe";
-    const loading = false;
-    const showName = true;
-
     return (
       <div className='App'>
-        {loading ? (
-          <h4>Loading...</h4>
-        ) : (
-          //<h1>Hello {showName ? name : null}</h1>
-          <h1>Hello {showName && name}</h1>
-        )}
+        <Navbar title='Github Finder' icon='fab fa-github' />
+        <div className='container'>
+          <Users />
+        </div>
       </div>
     );
   }
